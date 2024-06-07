f72cbff36866b9d3b41fc4ec6c1fa0f17330731b
shihannnn <shihan259@gmail.com>
2024-06-07 19:23:13 +0800

    Updated font sizing on smaller screens on SolvedCategory

--- src/components/SolvedCategory.tsx
+++ src/components/SolvedCategory.tsx
@@ -15,7 +15,7 @@ const SolvedCategory: React.FC<SolvedCategoryProps> = ({
     <div
       className={`${getColorClass(
         category
-      )} py-2 px-8 text-lg text-center rounded h-[75px] mb-2`}
+      )} py-2 px-8 xs:text-sm flex justify-center flex-col text-lg overflow-hidden leading-5 text-center rounded h-[75px] mb-2`}
     >
       <div className="font-bold">{answerItem.description.toUpperCase()}</div>
       <div>{answerItem.answer.join(", ").toUpperCase()}</div>
